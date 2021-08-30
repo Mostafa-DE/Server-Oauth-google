7bff0fc3375f3912dc1cd46c39dcd39334f93e94
Mostafa-DE <mostafafayyado1@gmail.com>
2021-08-30 17:55:53 +0300

    fix some error

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ app.get("/", (req, res) => {
   res.send("Home Page....");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About Page....");
-});
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server active on port ${PORT} :) `);
